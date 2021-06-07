7f8c041cc12439a54f7e4a017faa98cb6ebc730d
nathaliacardoso <nathalia.mscardoso@gmail.com>
2021-06-07 19:30:37 -0300

    Finalizar exercício 2

---
+++ semana18/src/index.ts
@@ -0,0 +1,40 @@
+import express, {Request, Response} from "express";
+import knex from "knex";
+import cors from 'cors'
+import {connection} from "./connection"
+import { AddressInfo } from "net";
+
+const app = express();
+
+app.use(express.json());
+
+type User = {
+    id : string,
+    name : string,
+    password : string,
+}
+
+app.post('/user',async(req:Request,res:Response)=>{
+    try {
+        const rb = req.body;
+        const newUser: User = {
+            id : rb.id,
+            name : rb.name,
+            password : rb.password
+        }
+        await connection('TBL_USER').insert(newUser)
+        res.status(200).send("created user successfuly!")
+    } catch (error) {
+        res.status(400).send({message:error.message})
+        console.log({message:error.message})
+    }
+})
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+})
